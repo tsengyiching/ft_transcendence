0dfdfbbd5abc5e8ad0b2c493f0fabcaff8c0040f
Arthur Bourbousson <abourbou@z3r6p5.42lyon.fr>
2021-08-13 11:30:36 +0200

    delete old files

--- front/src/utils/helper.js
+++
@@ -1,5 +0,0 @@
-export const server = {
-
-    baseURL: 'http://localhost:3000'
-    
-}
--- test/pouet.test.js
+++
@@ -1,9 +0,0 @@
-
-describe('pouet test', () => {
-  test('calling pouet return 12', () => {
-	  expect(12).toBe(12)
-  })
-  test('calling pouet return 42', () => {
-    expect(42).toBe(42)
-  })
-})
