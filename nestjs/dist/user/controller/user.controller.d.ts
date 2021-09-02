14b39004825e29bed2b1601c46c0fb641c3616da
tsengyiching <yictseng@student.42lyon.fr>
2021-09-02 17:47:48 +0200

    remove /dist

--- nestjs/dist/user/controller/user.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { Observable } from 'rxjs';
-import { UserService } from '../service/user.service';
-import { UserI } from '../models/user.interface';
-export declare class UserController {
-    private userService;
-    constructor(userService: UserService);
-    add(user: UserI): Observable<UserI>;
-    findAll(): Observable<UserI[]>;
-}
--- nestjs/dist/user/models/user.entity.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class UserEntity {
-    id: number;
-    name: string;
-}
