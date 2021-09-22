e9b075cd61f2ae6b11ef6dbc28c15190dded32b3
Felix Galaup <felixgalaup@yahoo.fr>
2021-09-22 13:00:41 +0200

    add dependency and add gateway to chat module

--- nestjs/src/chat/chat.module.ts
+++ nestjs/src/chat/chat.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
+import { GameModule } from 'src/game/game.module';
+import { UserModule } from 'src/user/user.module';
+import { ChanelGateway } from './gateway/chanel.gateway';
 
-@Module({})
+@Module({
+  imports: [UserModule, GameModule, AuthModule],
+  providers: [ChanelGateway],
+  exports: [ChatModule],
+})
 export class ChatModule {}
