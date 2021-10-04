cec3fabf87b6306bf6bea194d4b61a66d28179fc
Felix Galaup <felixgalaup@yahoo.fr>
2021-10-04 12:45:16 +0200

    fix import

--- nestjs/src/auth/auth.module.ts
+++ nestjs/src/auth/auth.module.ts
@@ -6,8 +6,8 @@ import { AuthController } from './controller/auth.controller';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
-import { FortyTwoStrategy } from '../strategy/42.strategy';
-import { JwtAuthStrategy } from '../strategy/jwt.strategy';
+import { FortyTwoStrategy } from './strategy/42.strategy';
+import { JwtAuthStrategy } from './strategy/jwt.strategy';
 
 @Module({
   imports: [
