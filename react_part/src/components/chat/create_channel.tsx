69aa9a8feebebbb4743e48a8ff2ffd84ccf6ad6c
Arthur Bourbousson <abourbou@z3r6p5.42lyon.fr>
2021-10-05 15:44:36 +0200

    button create channel finish

--- react_part/src/components/chat/create_channel.tsx
+++ react_part/src/components/chat/create_channel.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Modal, ToggleButton, ToggleButtonGroup, Form} from "react-bootstrap";
+import { Button, Modal, ToggleButton, ButtonGroup, ToggleButtonGroup, Form} from "react-bootstrap";
 
 interface Props {
   show: boolean,
@@ -9,7 +9,29 @@ interface Props {
 
 function CreateChannelModal(props: Props) {
 
-    const [passwordShow, setPasswordShow] = React.useState(false);
+    const [passwordLock, setPasswordLock] = useState(true);
+
+    function PrintPassword() {
+      if (passwordLock)
+      {
+        return (
+        <Form className="w-50 p-3">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" readOnly/>
+        </Form>
+        )
+      }
+      else
+      {
+        return (
+        <Form className="w-50 p-3">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password"/>
+        </Form>
+        )
+      }
+    }
+
   return (
     <Modal
       {...props}
@@ -23,20 +45,19 @@ function CreateChannelModal(props: Props) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <ToggleButtonGroup type="checkbox" defaultValue={[1, 2]}
-            className="mb-2">
-            <ToggleButton id="publicChannel" value={1} variant="primary">
+        <Form className="w-50 p-3">
+          <Form.Label> Channel Name </Form.Label>
+          <Form.Control type="channelName" placeholder="Channel Name"/>
+        </Form>
+        <ToggleButtonGroup name='typeChannel' className="mb-2" defaultValue={1} type='radio'>
+            <ToggleButton value ={1} id="publicChannel" type='radio' variant="primary" onClick={() => {setPasswordLock(true)}}>
               Public Channel
             </ToggleButton>
-            <ToggleButton id="privateChannel" value={2} variant="primary">
+            <ToggleButton value={2} id="privateChannel" type='radio' variant="primary" onClick={() => {setPasswordLock(false)}}>
               Private Channel
             </ToggleButton>
-          </ToggleButtonGroup>
-            {/*<Form>
-              <Form.Control type="text" readOnly>
-                Password
-              </Form.Control>
-            </Form>*/}
+        </ToggleButtonGroup>
+        <PrintPassword />
       </Modal.Body>
       <Modal.Footer>
         <Button variant="success" type="submit" onClick={props.onHide}>
@@ -45,7 +66,6 @@ function CreateChannelModal(props: Props) {
         <Button variant="secondary" onClick={props.onHide}>
             Cancel
         </Button>
-
       </Modal.Footer>
     </Modal>
   );
