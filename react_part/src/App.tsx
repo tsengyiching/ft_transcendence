3b0711667181c5819dfc3840615f8dcb02711a92
Arthur Bourbousson <abourbou@z3r6p5.42lyon.fr>
2021-10-11 17:22:46 +0200

    link sockets to channels

--- react_part/src/App.tsx
+++ react_part/src/App.tsx
@@ -2,13 +2,16 @@ import './App.css';
 import Router from './components/Router';
 import Header from './components/Header';
 import { Container } from 'react-bootstrap';
+import {SocketContext, socket} from './context/socket'
 
 function App() {
   return (
-    <Container fluid className="App">
-      <Header/>
-      <Router></Router>
-    </Container>
+    <SocketContext.Provider value={socket}>
+      <Container fluid className="App">
+        <Header/>
+        <Router></Router>
+      </Container>
+    </SocketContext.Provider>
   );
 }
 
