64b375efd23280fc34422c98c90b52b3a2ff5fdb
Fabien Buthod-garcon <fbuthod-@z3r6p3.42lyon.fr>
2021-10-13 16:00:52 +0200

    route added

--- react_part/src/components/Home.tsx
+++ react_part/src/components/Home.tsx
@@ -1,6 +1,11 @@
 import { Row, Col } from 'react-bootstrap'
 import {SocketContext} from "../context/socket"
 import { useContext } from 'react'
+import { Form, Button } from 'react-bootstrap'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+import Talk from './Talk';
 import Chat from './chat/Chat'
 import Game from './Game'
 import Friends from './Friends'
@@ -9,10 +14,27 @@ function Accueil() {
 
     let socket = useContext(SocketContext);
 
+function Accueil() {
+    const [id, setId] = useState(0);
+    const [name, setName] = useState("");
+
+    useEffect(() => {
+
+        axios.get('http://localhost:8080/profile/me/',{
+            withCredentials:true,
+        })
+        .then(res => {
+            setId(res.data.id)
+            setName(res.data.nickname)
+        })
+    })
     return (
         <div>
             <h2>Accueil</h2>
-                <Row>
+            <h3>{id}</h3>
+            <h3>{name}</h3>
+            <div className=".col-xl-">
+                <div className="row">
 					<Game />
                     <Col style={{border:'1px solid black'}}>
 						<Friends />
