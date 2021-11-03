12be08987e3f1246a25d09fecaa61f40727a1a77
Manami69 <lolopez@student.42lyon.fr>
2021-11-03 14:54:26 +0100

    halleluya

--- nestjs/src/pong/pong.env.ts
+++ nestjs/src/pong/pong.env.ts
@@ -1,10 +1,10 @@
 export const XR = 1;
 export const XL = 3;
 export const Y = 2;
-export const LEFTMAXANGLE = Math.PI * 5 / 12 // 75 deg to radian
+export const LEFTMAXANGLE = (Math.PI * 5) / 12; // 75 deg to radian
 export const LEFTLITTLEANGLE = Math.PI * 0.25;
-export const RIGHTMAXANGLE = Math.PI * 7 / 12 // 75 deg to radian
+export const RIGHTMAXANGLE = (Math.PI * 7) / 12; // 75 deg to radian
 export const RIGHTLITTLEANGLE = Math.PI * 0.75;
-export const H = 800 * 0.9 ;
+export const H = 800 * 0.9;
 export const W = 1000;
 export const FRAMERATE = 60;
