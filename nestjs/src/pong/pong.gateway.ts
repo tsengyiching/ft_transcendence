92b96c017d13ea00d202cf7aa85b1127d36b2837
tsengyiching <yictseng@student.42lyon.fr>
2021-11-03 16:25:25 +0100

    add auth service dans the pong gateway

--- nestjs/src/pong/pong.gateway.ts
+++ nestjs/src/pong/pong.gateway.ts
@@ -4,6 +4,9 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
+import { AuthService } from 'src/auth/service/auth.service';
+import { OnlineStatus, User } from 'src/user/model/user.entity';
+import { UserService } from 'src/user/service/user.service';
 import {
   gameInfos,
   Party,
@@ -15,8 +18,8 @@ import {
 import { PongService } from './pong.game.service';
 // https://www.generacodice.com/en/articolo/713202/how-can-i-find-the-response-time-latency-of-a-client-in-nodejs-with-sockets-socket-io
 const users: Player[] = [];
-let party: Party = setNewParty();
-let playing = false;
+const party: Party = setNewParty();
+const playing = false;
 
 @WebSocketGateway({
   namespace: 'pong',
@@ -27,109 +30,122 @@ let playing = false;
   },
 })
 export class PongGateway {
-  constructor(private pongService: PongService) {}
+  constructor(
+    private pongService: PongService,
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
   @WebSocketServer() server: any;
 
-  handleConnection(client: Socket, ...args: any[]) {
-    if (client.id) {
-      const newPlayer: Player = {
-        name: client.id,
-        id: users.length,
-        avatar: '',
-        paddle: 0,
-        score: 0,
-        up: false,
-        down: false,
-      };
-      users.push(newPlayer);
+  async handleConnection(client: Socket, ...args: any[]) {
+    try {
+      console.log('New Player Joins');
+
+      const user: User = await this.authService.getUserFromSocket(client);
+
+      //   if (client.id) {
+      //   const newPlayer: Player = {
+      //     name: client.id,
+      //     id: users.length,
+      //     avatar: '',
+      //     paddle: 0,
+      //     score: 0,
+      //     up: false,
+      //     down: false,
+      //   };
+      //   users.push(newPlayer);
+      // }
+      console.log(`User connected ${client.id}, ${users.length} connected`);
+      this.server.emit(
+        'message',
+        // `[${users
+        //   .map((e) => {
+        //     return e.name;
+        //   })
+        //   .join('] [')}]`,
+        `${user.nickname} est la!`,
+      );
+    } catch (error) {
+      console.log(error);
     }
-    console.log(`User connected ${client.id}, ${users.length} connected`);
-    this.server.emit(
-      'message',
-      `[${users
-        .map((e) => {
-          return e.name;
-        })
-        .join('] [')}]`,
-    );
   }
 
-  handleDisconnect(client: Socket) {
-    const index = users
-      .map((e) => {
-        return e.name;
-      })
-      .indexOf(client.id);
-    if (users[index].paddle !== 0) playing = false;
-    if (index > -1) {
-      users.splice(index, 1);
-    }
-    console.log(`User disconnected ${client.id}`);
-    this.server.emit('message', `[${users.join('] [')}]`);
-  }
-  @SubscribeMessage('down')
-  onDown(client: any, payload: any) {
-    users.map((e) => {
-      if (e.name === client.id) e.down = payload;
-    });
-  }
-  @SubscribeMessage('up')
-  onUp(client: any, payload: any) {
-    users.map((e) => {
-      if (e.name === client.id) e.up = payload;
-    });
-  }
+  // handleDisconnect(client: Socket) {
+  //   const index = users
+  //     .map((e) => {
+  //       return e.name;
+  //     })
+  //     .indexOf(client.id);
+  //   if (users[index].paddle !== 0) playing = false;
+  //   if (index > -1) {
+  //     users.splice(index, 1);
+  //   }
+  //   console.log(`User disconnected ${client.id}`);
+  //   this.server.emit('message', `[${users.join('] [')}]`);
+  // }
+  // @SubscribeMessage('down')
+  // onDown(client: any, payload: any) {
+  //   users.map((e) => {
+  //     if (e.name === client.id) e.down = payload;
+  //   });
+  // }
+  // @SubscribeMessage('up')
+  // onUp(client: any, payload: any) {
+  //   users.map((e) => {
+  //     if (e.name === client.id) e.up = payload;
+  //   });
+  // }
 
-  @SubscribeMessage('sub')
-  subscribe(client: Socket, payload: number) {
-    console.log(client.id, payload);
-    if (
-      users
-        .map((e) => {
-          return e.paddle;
-        })
-        .indexOf(payload) >= 0
-    )
-      return;
-    const index = users
-      .map((e) => {
-        return e.name;
-      })
-      .indexOf(client.id);
-    if (users[index].paddle != 0) return;
-    users[index].paddle = payload;
-    const other = payload === 1 ? 2 : 1;
+  // @SubscribeMessage('sub')
+  // subscribe(client: Socket, payload: number) {
+  //   console.log(client.id, payload);
+  //   if (
+  //     users
+  //       .map((e) => {
+  //         return e.paddle;
+  //       })
+  //       .indexOf(payload) >= 0
+  //   )
+  //     return;
+  //   const index = users
+  //     .map((e) => {
+  //       return e.name;
+  //     })
+  //     .indexOf(client.id);
+  //   if (users[index].paddle != 0) return;
+  //   users[index].paddle = payload;
+  //   const other = payload === 1 ? 2 : 1;
 
-    if (
-      users
-        .map((e) => {
-          return e.paddle;
-        })
-        .indexOf(other) >= 0
-    ) {
-      playing = true;
-      this.server.emit('go', '');
-    }
-    console.log(playing);
-  }
-  @SubscribeMessage('gameon')
-  gameOn(client: any, payload: any) {
-    if (playing) {
-      console.log('JEU');
-      party = setNewParty();
-      this.server.emit('gameOk', true);
-    } else if (!playing) this.server.emit('gameOk', false);
-    else;
-  }
+  //   if (
+  //     users
+  //       .map((e) => {
+  //         return e.paddle;
+  //       })
+  //       .indexOf(other) >= 0
+  //   ) {
+  //     playing = true;
+  //     this.server.emit('go', '');
+  //   }
+  //   console.log(playing);
+  // }
+  // @SubscribeMessage('gameon')
+  // gameOn(client: any, payload: any) {
+  //   if (playing) {
+  //     console.log('JEU');
+  //     party = setNewParty();
+  //     this.server.emit('gameOk', true);
+  //   } else if (!playing) this.server.emit('gameOk', false);
+  //   else;
+  // }
 
-  @SubscribeMessage('start')
-  startGame(client: Socket, Payload: any) {
-    const IntervalID = setInterval(() => {
-      party = UpdateGame(users, party);
-      client.volatile.emit('infos', gameInfos(party));
-      if (playing === false) {
-        clearInterval(IntervalID);
-      }
-    }, 1000 / FRAMERATE);
-  }
+  // @SubscribeMessage('start')
+  // startGame(client: Socket, Payload: any) {
+  //   const IntervalID = setInterval(() => {
+  //     party = UpdateGame(users, party);
+  //     client.volatile.emit('infos', gameInfos(party));
+  //     if (playing === false) {
+  //       clearInterval(IntervalID);
+  //     }
+  //   }, 1000 / FRAMERATE);
+  // }
 }
