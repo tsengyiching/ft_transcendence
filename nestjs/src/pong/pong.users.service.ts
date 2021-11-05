03ffa6a175bed49d9c61752cc6cc9c800cf5c4b8
Manami69 <lolopez@student.42lyon.fr>
2021-11-05 18:55:19 +0100

    avancee dans le backend

--- nestjs/src/pong/pong.users.service.ts
+++ nestjs/src/pong/pong.users.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PongUsersService {}
+export class PongUsersService {
+  private inMatchMaking: number[] = [];
+  private isConnected: number[] = [];
+
+  isInMatchmaking(id: number): boolean {
+    const found = this.inMatchMaking.find((e) => e === id);
+    return found ? true : false;
+  }
+  addNewPlayer(id: number): boolean {
+    if (!this.isInMatchmaking(id)) {
+      this.inMatchMaking.push(id);
+      return false;
+    }
+    return true;
+  }
+
+  removePlayer(id: number) {
+    const found = this.inMatchMaking.indexOf(id);
+    if (found !== -1) {
+      this.inMatchMaking.splice(found, 1);
+    }
+  }
+
+  userConnect(id: number) {
+    this.isConnected[id] += 1;
+  }
+
+  userDisconnect(id: number): number {
+    this.isConnected[id] -= 1;
+    return this.isConnected[id];
+  }
+
+  nbWindowForUser(id: number): number {
+    return this.isConnected[id];
+  }
+}
--- react_part/src/components/pong/components/Fetch.tsx
+++ react_part/src/components/pong/components/Fetch.tsx
@@ -2,7 +2,7 @@ import { useEffect , useState, useContext} from "react";
 import axios from 'axios';
 import io, {Socket} from 'socket.io-client';
 import useKeyPressCB from "../hooks/UseKeyPressCB";
-import { GameSocketContext } from "../context/gameSocket";
+import { GameSocketContext } from "../../../context/gameSocket";
 import SocketEvent from "./SocketEvent";
 import SocketInfos from "./SocketInfos";
 import Pong from "./Pong";
