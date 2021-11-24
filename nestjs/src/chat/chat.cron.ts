23ce8c6bc61bbfdaa517f6182ccd19f42034bcc1
Felix Galaup <felixgalaup@yahoo.fr>
2021-11-24 13:05:41 +0100

    fix send message event

--- nestjs/src/chat/chat.cron.ts
+++ nestjs/src/chat/chat.cron.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
