2c2e4720bcacf05efc6b9f36c00ed3a13bfbdd33
tsengyiching <yictseng@student.42lyon.fr>
2021-12-06 16:31:25 +0100

    add socket in admin/set api; give channel right (read msgs and get channel users)for site admin

--- nestjs/src/admin/admin.module.ts
+++ nestjs/src/admin/admin.module.ts
@@ -1,12 +1,13 @@
 import { Inject, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChatModule } from 'src/chat/chat.module';
 import { User } from 'src/user/model/user.entity';
 import { UserService } from 'src/user/service/user.service';
 import { UserModule } from 'src/user/user.module';
 import { AdminController } from './admin.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), UserModule],
+  imports: [TypeOrmModule.forFeature([User]), UserModule, ChatModule],
   controllers: [AdminController],
 })
 export class AdminModule {
