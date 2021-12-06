c22f8c2d2a22b64f2719592d47dc72ecc1f6168a
Felix Galaup <felixgalaup@yahoo.fr>
2021-12-06 14:32:46 +0100

    Update nav bar and rework

--- react_part/src/components/web_pages/Header.tsx
+++ react_part/src/components/web_pages/Header.tsx
@@ -9,7 +9,7 @@ import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 import {LinkContainer} from 'react-router-bootstrap';
-import logoPong from '../pictures/ping-pong.png';
+import logoPong from './pictures/ping-pong.png';
 
 function Header () {
     const [isConnected, setConnexion] = useState(true);
