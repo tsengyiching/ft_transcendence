ea7eee59e21c4a1d1fc1bd0cec54a8bfb825febc
tsengyiching <yictseng@student.42lyon.fr>
2021-12-10 17:53:45 +0100

    add http status forbidden for user banned from site.

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -5,8 +5,8 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser()); // Do you like cookies ?
-  app.useGlobalPipes(new ValidationPipe()); // validation for every incoming requests
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     credentials: true,
     origin: 'http://localhost:3000',
