39c28d88164d91f564dcb84115da807e172cc2ee
tsengyiching <yictseng@student.42lyon.fr>
2021-12-10 12:33:03 +0100

    add client.emit alert to pong gateway

--- nestjs/src/pong/pong.gateway.ts
+++ nestjs/src/pong/pong.gateway.ts
@@ -47,7 +47,7 @@ export class PongGateway {
       const resp = this.pongUsersService.isInMatchmaking(user.id);
       client.emit('inMatchMaking', resp);
     } catch (error) {
-      console.log(error);
+      client.emit(`alert`, { alert: { type: `danger`, message: error.error } });
     }
   }
 
@@ -63,7 +63,7 @@ export class PongGateway {
         this.pongUsersService.removePlayer(user.id);
       }
     } catch (error) {
-      console.log(error);
+      client.emit(`alert`, { alert: { type: `danger`, message: error.error } });
     }
   }
   /**
