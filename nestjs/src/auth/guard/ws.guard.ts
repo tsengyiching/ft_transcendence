8721f59b7bdd2f67deeabdee3e16925315070bd3
tsengyiching <yictseng@student.42lyon.fr>
2021-12-15 13:56:54 +0100

    add twofa in wsguard; add two-factor cookie in turn-on api

--- nestjs/src/auth/guard/ws.guard.ts
+++ nestjs/src/auth/guard/ws.guard.ts
@@ -13,15 +13,27 @@ export class WsGuard implements CanActivate {
     const bannedIds = await this.userService.getBannedUserIds();
     const bearerToken: string = context.switchToHttp().getRequest().handshake
       .headers.cookie;
-    const niddle = bearerToken.indexOf('jwt=');
-    const jwt = bearerToken.slice(niddle + 4);
+    const jwt = 'jwt=';
+    const twofa = 'jwt-two-factor=';
+    const niddleJWT = bearerToken.indexOf(jwt);
+    const niddleTwoFA = bearerToken.indexOf(twofa);
+    let jwtToken: string, twofaToken: string;
+    if (niddleJWT === -1) {
+      jwtToken = bearerToken.slice(niddleJWT + jwt.length);
+      twofaToken = null;
+    } else {
+      jwtToken = bearerToken.slice(niddleJWT + jwt.length, niddleTwoFA - 2);
+      twofaToken = bearerToken.slice(niddleTwoFA + twofa.length);
+    }
     try {
-      const user = await this.authService.getUserFromAuthenticationToken(jwt);
-      if (user && !bannedIds.includes(user.id)) {
-        return true;
-      } else {
-        return false;
+      const user = await this.authService.getUserFromAuthenticationToken(
+        jwtToken,
+      );
+      if (twofaToken) {
+        await this.authService.getUserFromAuthenticationToken(twofaToken);
       }
+      if (user && !bannedIds.includes(user.id)) return true;
+      else return false;
     } catch (err) {
       return false;
     }
