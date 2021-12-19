8a7fc2ed6283dfef47d633a0417a6f94536bae7c
Felix Galaup <felixgalaup@yahoo.fr>
2021-12-19 15:09:12 +0100

    add new event to reload-blockedby.

--- nestjs/src/relationship/controller/relationship.controller.ts
+++ nestjs/src/relationship/controller/relationship.controller.ts
@@ -172,7 +172,9 @@ export class RelationshipController {
       relationshipDto,
     );
     this.chatGateway.server.to('user-' + user.id).emit('reload-block');
-    this.chatGateway.server.to('user-' + relationshipDto.addresseeUserId).emit('reload-blockedby');
+    this.chatGateway.server
+      .to('user-' + relationshipDto.addresseeUserId)
+      .emit('reload-blockedby');
     this.chatGateway.server
       .to('user-' + relationshipDto.addresseeUserId)
       .emit('reload-blockedby');
@@ -197,7 +199,9 @@ export class RelationshipController {
       relationshipDto,
     );
     this.chatGateway.server.to('user-' + user.id).emit('reload-block');
-    this.chatGateway.server.to('user-' + relationshipDto.addresseeUserId).emit('reload-blockedby');
+    this.chatGateway.server
+      .to('user-' + relationshipDto.addresseeUserId)
+      .emit('reload-blockedby');
     this.chatGateway.server
       .to('user-' + relationshipDto.addresseeUserId)
       .emit('reload-blockedby');
