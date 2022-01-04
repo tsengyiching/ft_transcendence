8abf1da2071e9e7275c5ca5d3f253e0bc98a5ab1
Manami69 <lolopez@student.42lyon.fr>
2022-01-04 15:55:57 +0100

    resize

--- react_part/src/components/pong/components/ResizeGame.tsx
+++ react_part/src/components/pong/components/ResizeGame.tsx
@@ -5,8 +5,8 @@ import { useMediaQuery } from 'react-responsive'
 const ResizeGame:React.FC<{}> = () => {
 	const set = useStore.setState;
     const w = useStore(s => s.w);
-    const handleMediaQueryChange = (matches:boolean) => {
-        if (!matches && w !== 750) {
+    const handleSecondStep = (matches:boolean) => {
+        if (matches && w !== 700) {
         set(s => ({...s,
             h: 800 *0.75 * 0.9,
             w: 1000 * 0.75,
@@ -16,22 +16,10 @@ const ResizeGame:React.FC<{}> = () => {
                 w : 1000*0.75,
             }
 			}));}
-
-            else if (matches && w!== 1000){
-                set(s => ({...s,
-                    h: 800 * 0.9,
-                    w: 1000,
-                    scoreBar: {
-                        ...s.scoreBar,
-                        h: 800 * 0.1,
-                        w : 1000,
-                    }
-                    }));
-            }
-          }
+    }
       
-        const handleLittle = (matches:boolean) => {
-            if (!matches && w !== 500) {
+    const handleLittle = (matches:boolean) => {
+            if (matches && w !== 500) {
                 set(s => ({...s,
                 h: 800 *0.5 * 0.9,
                 w: 1000 * 0.5,
@@ -41,29 +29,53 @@ const ResizeGame:React.FC<{}> = () => {
                     w : 1000*0.5,
                 }
                 }))
-            ;}
-            else if (matches && w!== 750){
-                set(s => ({...s,
-                    h: 800 * 0.75* 0.9,
-                    w: 1000* 0.75,
-                    scoreBar: {
-                        ...s.scoreBar,
-                        h: 800 * 0.75* 0.1,
-                        w : 1000* 0.75,
-                    }
-                    }));
-            }
-        }
-  
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const isBig = useMediaQuery(
-        { minWidth: 1200 }, undefined,  handleMediaQueryChange
-      );
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const isDesktopOrLaptop = useMediaQuery(
-        { minWidth: 780 }, undefined,  handleLittle
-      );
+        ;}
+    }
 
+	const handleVeryLittle = (matches:boolean) => {
+		if (matches && w !== 400) {
+			set(s => ({...s,
+			h: 800 *0.4 * 0.9,
+			w: 1000 * 0.4,
+			scoreBar: {
+				...s.scoreBar,
+				h: 800 *0.4 * 0.1,
+				w : 1000*0.4,
+			}
+			}))
+	;}
+}
+
+		const handleFirstStep = (matches:boolean) => {
+				if (matches && w!== 1000){
+					set(s => ({...s,
+						h: 800 * 0.9,
+						w: 1000,
+						scoreBar: {
+							...s.scoreBar,
+							h: 800 * 0.1,
+							w : 1000,
+						}
+						}));
+				}
+		}
+
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const  firstStep = useMediaQuery(
+			{ minWidth: 1636 }, undefined, handleFirstStep
+		);
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const secondStep = useMediaQuery(
+        { minWidth: 1198, maxWidth:1636 }, undefined,  handleSecondStep
+      );
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const isDesktopOrLaptop = useMediaQuery(
+        { minWidth: 780, maxWidth:1198 }, undefined,  handleLittle
+      );
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const veryLittle = useMediaQuery(
+			{ maxWidth:780 }, undefined,  handleVeryLittle
+		  );
 	
 
 return null;}
